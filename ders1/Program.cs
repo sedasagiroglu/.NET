9ee85e7af4166c397f30714a6dc2758f8b9f9d1e
SEDA <37578347+sedasagiroglu@users.noreply.github.com>
2023-03-17 22:21:47 +0300

    Odev1

---
+++ ders1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] numbers = { 13, 46, 0, 1, 18, -9 };
+
+Console.WriteLine(numbers[0]);
+
+Console.WriteLine(numbers.Length);
+
+Array.Sort(numbers);      //artan düzene göre sırala
+foreach (int i in numbers) // herbiri için 
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine(numbers.Max()); 
+Console.WriteLine(numbers.Min());  
+Console.WriteLine(numbers.Sum());
+Console.WriteLine(numbers.Average()); // ortalama 
