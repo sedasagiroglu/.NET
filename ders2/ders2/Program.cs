f2c67d822c82d90ce0d68b46f4c7ba2f6e014b02
SEDA <37578347+sedasagiroglu@users.noreply.github.com>
2023-03-18 23:15:17 +0300

    odev-2
    Inheritance

---
+++ ders2/ders2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+deneme-1
+n kenarlı düzgün çokgen:"n" tane kenara sahip ve her kenarın ve açının birbirine eşit olduğu çokgendir.
+DuzgunCokgen adında bir sınıf oluşturalım. Bu sınıfın aşağıdaki özelliklere sahip olsun.
+
+*Çokgenin kenar sayısını tutan "n" adında bir değişken (int/varsayılan değer=3)
+*Çokgenin bir kenar uzunluğunu tutan "kenar" adında bir değişken (double/varsayılan değer=1)
+*Çokgenin merkezinin x-koordinatını tutan "x" adında bir değişken (double/varsayılan değer=0)
+*Çokgenin merkezinin y-koordinatını tutan "y" adında bir değişken (double/varsayılan değer=0)
+*Varsayılan değerlerle çokgen oluşturan yapılandırıcı olsun 
+*Merkezi(0,0), belirli sayıda kenarı ve belirli kenar uzunluğu olan bir çokgen oluşturan yapılandırıcı.
+*Belirli sayıda kenarı, belirli kenar uzunluğu ve merkezi olan bir çokgen oluşturan yapılandırıcı
+*Her bir alan için set/get metodları oluştur
+*"Çevre" adında çokgenin çevresini döndüren bir metod
+*"Alan" adında çokgenin alanını döndüren bir metod
+
+************************************************************************************************************************
+deneme-2
+*Bir tane öğrenci sınıfı oluşturun.Sınıf değişkenleri:fakulte, bolum, cinsiyet, numara ve not olsun 
+*İki tane yapılandırıcı olsun, biri yukardaki değişkenleri parametre olarak alsın, diğeri sadece fakulte ve bolum
+*goster adında bir metod olsun öğrencinin bütün bilgilerini yazdırsın.
+*/
